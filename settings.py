0565542b3ac4dddbedb2b39d80ea682889379c62
Alexandra <matildakrokodilda@gmail.com>
2023-06-09 18:22:15 +0300

    база данны, картинки

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
 ENTER = ''
 RESULT = 0
 PICTURE = 'https://tipik.ru/wp-content/uploads/2019/08/%D0%9B%D1%83%D1%87%D1%88%D0%B8%D0%B5-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-%D1%8D%D1%81%D0%BA%D0%B8%D0%B7%D1%8B-%D0%BA%D0%BE%D1%82%D0%B0-%D0%BA%D0%BE%D0%BB%D0%BB%D0%B5%D0%BA%D1%86%D0%B8%D1%8F-39.jpg'
+PICTURE_RIGHT = 'https://koshka.top/uploads/posts/2021-12/thumbs/1639015295_3-koshka-top-p-dovolnii-kotik-3.jpg'
+PICTURE_WRONG = 'https://vetinfo.com.ua/sites/default/files/styles/flexslider_full/public/20143010160217.jpg?itok=YsOG5SD1'
